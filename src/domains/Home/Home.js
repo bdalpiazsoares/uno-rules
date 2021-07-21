c0aaba35be8e60e4728aabfbd9c922a9440116f2
bdalpiazsoares <bdalpiazsoares@gmail.com>
2021-07-21 13:16:01 -0300

    fix position button

--- src/domains/Home/Home.js
+++ src/domains/Home/Home.js
@@ -4,16 +4,18 @@ import { homeTextFirst, homeTextSecond } from '../../utils/utils'
 import styles from './home.module.scss'
 
 export const Home = () => {
-  const { containerHistory, title, text, sectionContainer, containerButton } = styles
+  const { containerHistory, title, text, sectionContainer, containerButton, containerTest } = styles
 
   return (
     <section className={sectionContainer} id='home'>
       <div className={containerHistory}>
-        <h1 className={title}>Todos gostamos de uma boa partida de UNO não é mesmo?</h1>
-        <small className={text}>{homeTextFirst}</small>
-        <small className={text}>{homeTextSecond}</small>
-        <div className={containerButton}>
-          <Button name='Confira as regras' navigateTo='rules' />
+        <div className={containerTest}>
+          <h1 className={title}>Todos gostamos de uma boa partida de UNO não é mesmo?</h1>
+          <p className={text}>{homeTextFirst}</p>
+          <p className={text}>{homeTextSecond}</p>
+          <div className={containerButton}>
+            <Button name='Confira as regras' navigateTo='rules' />
+          </div>
         </div>
       </div>
     </section>
